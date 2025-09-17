7c8532c7c246dcf545f84a382bf1a9e47fe1d65d
Karol Mikołajewski <karolmikolajewski@wp.pl>
2025-09-17 13:58:54 +0200

    start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -11,6 +11,86 @@ app.get('/', (req, res) => {
 
 
 
+const users = [];
+app.post('/api/users', (req, res) => {
+  let username = req.body.username;
+  if (!username) {
+    return res.json({ error: 'Username is required' });
+  }
+  if (users.find(u => u.username === username)) {
+    return res.json({ error: 'Username already taken' });
+  }else{
+  const newUser = { username: username , _id: (users.length + 1).toString() };
+  users.push(newUser);
+  res.json(newUser);
+  }
+});
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  let userId = req.params._id;
+  let user = users.find(u => u._id === userId);
+  if(!user){
+    return res.json({ error: 'User not found'})
+  }
+  let description = req.body.description.toString();
+  let duration = req.body.duration;
+  let date = req.body.date ? new Date(req.body.date) : new Date();
+  if (isNaN(date.getTime())) {
+    return res.json({ error: 'Invalid date format' });
+  }
+  if (!description || !duration) {
+    return res.json({ error: 'Description and duration are required' });
+  }
+  const exercise = {
+    description: description.toString(),
+    duration: parseInt(duration),
+    date: date.toDateString(),
+  };
+  if (!user.log) {
+    user.log = [];
+  }
+  user.log.push(exercise);
+  console.log('/api/users/:_id/exercises', {
+    username: user.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date,
+    _id: user._id,
+  })
+
+  res.json({
+    username: user.username,
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date,
+    _id: user._id,
+  });
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  let userId = req.params._id;
+  let user = users.find(u => u._id === userId);
+  if(!user){
+    return res.json({ error: 'User not found'})
+  }
+  console.log('/api/users/:_id/logs', {
+    username: user.username,
+    count: user.log?.length,
+    _id: user._id,
+    log: user.log ? user.log : []
+  })
+  res.json({
+    username: user.username,
+    count: user.log?.length,
+    _id: user._id,
+    log: user.log ? user.log : []
+  })
+
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
